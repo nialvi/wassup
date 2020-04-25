f52e93e009decc60c4668ff76a598e31ce6e1940
Nikolaev Viktor <maclauddd@gmail.com>
2020-04-25 22:33:11 +0300

    added test for report view

---
+++ src/app/report/view/__tests__/index.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { render } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { RouterProvider } from "react-router5";
+import Report from "..";
+import { store } from "../../../Page/store";
+import createRouter from "../../../Page/route/createRouter";
+
+describe("Report view", () => {
+  // TODO fix this test
+  // it("render report without router provider", () => {
+  //   const { getByText } = render(<Report />);
+
+  //   expect(getByText("report")).toBeInTheDocument();
+  // });
+
+  it("render report without router provider", () => {
+    const router = createRouter(store);
+
+    router.start("/report");
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <RouterProvider router={router}>
+          <Report />
+        </RouterProvider>
+      </Provider>
+    );
+
+    expect(getByText("report")).toBeInTheDocument();
+  });
+
+  it("render report without router provider", () => {
+    const router = createRouter(store);
+
+    router.start("/report/all");
+
+    const { getByText } = render(
+      <Provider store={store}>
+        <RouterProvider router={router}>
+          <Report />
+        </RouterProvider>
+      </Provider>
+    );
+
+    expect(getByText("all reports")).toBeInTheDocument();
+  });
+});
--- src/app/report/view/index.tsx
+++ src/app/report/view/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useRouteNode } from "react-router5";
 
 const Report = () => {
-  const { route } = useRouteNode("report");
+  const { route } = useRouteNode("report") || {};
 
   if (route.name === "report.all") {
     return <div>all reports</div>;
