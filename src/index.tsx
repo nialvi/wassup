b91549f2113b6bd62e7131df21545f799517e1ea
Nikolaev Viktor <maclauddd@gmail.com>
2020-04-29 07:14:48 +0300

    draft added event in indexeddb

--- src/index.tsx
+++ src/index.tsx
@@ -27,18 +27,62 @@ router.start(() => {
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
 
-const request = window.indexedDB.open("MyTestDB", 2);
+const requestOpenDB = window.indexedDB.open("MyTestDB", 3);
 
-request.onerror = function (event) {
+requestOpenDB.onerror = function (event) {
   console.error("error", event);
 };
 
-request.onsuccess = function (event: any) {
+requestOpenDB.onsuccess = function () {
+  const db = requestOpenDB.result;
+
+  const transaction = db.transaction("events", "readwrite");
+
+  let eventsTransaction = transaction.objectStore("events");
+
+  let event = {
+    id: "meetup_standup",
+    category: "meetup",
+    title: "standup",
+    description: [
+      "This is example event with templated description",
+      "This is additional description",
+    ],
+    date: "2020-03-30",
+  };
+
+  let requestAddEvent = eventsTransaction.put(event);
+
+  requestAddEvent.onsuccess = function (event) {
+    console.log("event added", requestAddEvent.result);
+  };
+
+  requestAddEvent.onerror = function (event) {
+    console.log("error", requestAddEvent.error);
+  };
+
+  db.onversionchange = function () {
+    db.close();
+    alert("Database is outdated, please reload the page.");
+  };
+
   console.log("indexedDB is successed work");
 };
 
-request.onupgradeneeded = function (event) {
-  const db = request.result;
-  db.createObjectStore("events");
+requestOpenDB.onupgradeneeded = function (event) {
+  const db = requestOpenDB.result;
+
+  if (!db.objectStoreNames.contains("events")) {
+    db.createObjectStore("events", { keyPath: "id" });
+  }
+
+  if (!db.objectStoreNames.contains("reports")) {
+    db.createObjectStore("reports", { keyPath: "id" });
+  }
+
   console.log("onupgrade", event);
+};
+
+requestOpenDB.onblocked = function () {
+  // handle the case when an old connection wasn't closed after db.onversionchange triggered
 };
