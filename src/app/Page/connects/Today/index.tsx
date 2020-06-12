86c805685496fd0cf3a65c3d74755cf3d16eaa97
Nikolaev Viktor <maclauddd@gmail.com>
2020-06-13 00:01:54 +0300

    filtered only today events

--- src/app/Page/connects/Today/index.tsx
+++ src/app/Page/connects/Today/index.tsx
@@ -1,12 +1,21 @@
 import { connect } from "react-redux";
+import { format } from "date-fns";
 import Today from "../../../Event/view/Today";
 import { addEvent } from "../../../Event/entity/actionsCreators";
 import { State } from "../../usecases/reducers";
 
-const mapStateToProps = (state: State) => ({
-  // TODO add selector
-  events: Object.keys(state.events).map((id) => state.events[id]),
-});
+const mapStateToProps = (state: State) => {
+  const { events } = state;
+  const today = format(new Date(), "yyyy-MM-dd");
+  const todayEvents = Object.keys(events)
+    .map((id) => events[id])
+    .filter((event) => event.date === today);
+
+  return {
+    // TODO add selector
+    events: todayEvents,
+  };
+};
 
 const mapDispatchToProps = {
   addEvent,
